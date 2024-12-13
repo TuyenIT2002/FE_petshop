bd3918db104986b692e6ad789a5e397158d6ec1b
TuyenIT2002 <tuyenlinh15081405@gmail.com>
2024-12-13 21:05:38 +0700

    New-Product

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,16 @@
 import Banner from '~/Layout/components/Banner';
 import InfoBar from '~/Layout/components/InfoBar';
 import FeatureProduct from '~/Layout/components/FeatureProduct';
+import NewProduct from '~/Layout/components/NewProduct';
+import BestSellerAndLike from '~/Layout/components/BestSellerAndLike';
 function Home() {
     return (
         <>
             <Banner />
             <InfoBar />
             <FeatureProduct />
+            <NewProduct />
+            <BestSellerAndLike />
         </>
     );
 }
